d13d43fef90d10c9d5939ab31c81aa70ac7c06c3
OmarEduS <oesl96@gmailcom>
2021-02-23 10:57:50 -0600

    Testing Commit Insturction to Git

---
+++ Instruction.java
@@ -0,0 +1,30 @@
+public class Instruction {
+    //Variables
+    String name;
+    String hex;
+
+    public Instruction(String name, String hex) {
+        this.name = name;
+        this.hex = hex;
+    }
+
+    public void putName(String name) {
+        this.name = name;
+    }
+
+    public void putHex(String hex) {
+        this.hex = hex;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getHex(){
+        return hex;
+    }
+
+    public void printInst() {
+        System.out.println(name + " " + hex);
+    }
+}
